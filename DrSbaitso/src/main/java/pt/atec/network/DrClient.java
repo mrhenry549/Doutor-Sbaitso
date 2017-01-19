55ec1a7f5059b933ca2a977cec6c31007714f50f
Henrique <Henrique@192.168.250.186>
2017-01-19 11:25:55 +0000

    (não sei)º commit

--- DrSbaitso/src/main/java/pt/atec/network/DrClient.java
+++ DrSbaitso/src/main/java/pt/atec/network/DrClient.java
@@ -1,32 +1,62 @@
 package pt.atec.network;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
-import pt.atec.drsbaitso.Inteligence;
 
+/**
+ *
+ * @author Userpl022pc19
+ */
 public class DrClient {
-
+    
     private final int PORTO = 9983;
-    private final String IP = "192.168.250.231";
+    private final String IP = "192.168.0.54";
     
     PrintWriter out;
     BufferedReader in;
-    Inteligence mind;
     
-    int num = 1;
+    OutputStream os;
+    InputStream is;
 
     public DrClient() {
-        mind = new Inteligence();
 
         try {
 
-            Socket socket = new Socket(IP, PORTO);
+            Socket ssoc = new Socket(IP, PORTO);
+            
 
-            out = new PrintWriter(socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            /*out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));*/
+            
+            os = ssoc.getOutputStream();
+            is = ssoc.getInputStream();
+            
+            String st = "Esta turma é espetacular";
+            byte[] bs = st.getBytes();
+            
+            
+            
+            os.write(bs);
+            
+            byte bin = 0;
+            ArrayList<Byte> arb = new ArrayList<Byte>();
+            
+            while (bin >= 0){
+                bin = (byte) is.read();
+                arb.add(bin);
+            }
+            
+           byte[] b = new byte[arb.size()];
+           
+           for(int i = 0; i < arb.size(); i++){
+               b[i] = arb.get(i);
+           }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -37,16 +67,16 @@ public class DrClient {
 
         try {
             out.println();
-            while(num != 0){
+            
             inputLine = in.readLine();
             
             System.out.println(inputLine);
             
-            //System.out.println("Bajule o seu Servidor");
+            System.out.println("Bajule o seu Servidor");
             String msg = (new Scanner(System.in)).nextLine();
-            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
+    
 }
--- DrSbaitso/src/main/java/pt/atec/network/DrServer.java
+++ DrSbaitso/src/main/java/pt/atec/network/DrServer.java
@@ -1,53 +1,85 @@
 package pt.atec.network;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
-import pt.atec.drsbaitso.Inteligence;
 
+/**
+ *
+ * @author Userpl022pc19
+ */
 public class DrServer {
-
+    
     private final int PORTO = 9983;
     
     PrintWriter out;
     BufferedReader in;
-    Inteligence mind;
-    String inputLine, outputLine, msg;
-    int num = 1;
+    
+    OutputStream os;
+    InputStream is;
 
     public DrServer() {
-        mind = new Inteligence();
-        
-        String greet = mind.getGreeting();
 
         try {
             ServerSocket ssoc = new ServerSocket(PORTO);
-
+            
             Socket client = ssoc.accept();
 
-            out = new PrintWriter(client.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            os = client.getOutputStream();
+            is = client.getInputStream();
+            
+           /* byte b = (byte) 133;
+            os.write(b);
+            
+            byte bin = (byte) is.read();*/
+            
+            String st = "Esta turma é espetacular";
+            byte[] bs = st.getBytes();
+            
+            
+            
+            os.write(bs);
+            
+            byte bin = 0;
+            ArrayList<Byte> arb = new ArrayList<Byte>();
+            
+            while (bin >= 0){
+                bin = (byte) is.read();
+                arb.add(bin);
+            }
+            
+           byte[] b = new byte[arb.size()];
+           
+           for(int i = 0; i < arb.size()-1; i++){
+               b[i] = arb.get(i);
+           }
+            
+            /*System.out.println("Byte Recebido: " + bin);
+            JOptionPane.showMessageDialog(null, "O número foi: " + bin);*/
+
+            /*out = new PrintWriter(client.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));*/
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
+        String inputLine, outputLine;
 
         try {
             out.println("bom dia");
-            while(num != 0){
-                inputLine = in.readLine();
+            
+            inputLine = in.readLine();
 
-                System.out.println(inputLine);
-
-                msg = (new Scanner(System.in)).nextLine();
-
-                out.println(msg);
-            }
+            System.out.println(inputLine);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
+    
 }
